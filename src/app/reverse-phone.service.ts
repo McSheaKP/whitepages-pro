38eab31965da53d9eed4735b05b4dd13fce92fed
Kevin McShea <KevinMcShea@gmail.com>
2018-05-05 12:43:06 -0700

    updated html and Http breakup

--- src/app/reverse-phone.service.ts
+++ src/app/reverse-phone.service.ts
@@ -3,17 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Observable} from 'rxjs/Observable';
 
 
-
-
-
-
 @Injectable()
 export class ReversePhoneService {
- url = 'https://proapi.whitepages.com/3.0/phone?phone=4848682340&api_key=b0ead43fa0714b2ebf7ea1a31f88e66e'
-   
+ 
+  urlBeginning: String: 'https://proapi.whitepages.com/3.0/phone?phone=';
+  userPhoneNum: String: "4848682340";
+  apiKey: String: '&api_key=b0ead43fa0714b2ebf7ea1a31f88e66e';
+  url =  this.urlBeginning + this.userPhoneNum + this.apiKey;
+
   constructor(private http: HttpClient) { }
-  
-  userPhoneNum: Number: 0;
+
 
   getData (){
     return this.http.get(this.url)
